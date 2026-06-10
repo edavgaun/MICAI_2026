24479451535c661d5388866b4f0cc517d5b55b0e
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2026-06-10 15:39:07 -0500

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,13 @@
 import pandas as pd
-import os
+import streamlit as st
 
 DATA_PATH = "data/metadata_extraida.csv"
 
+@st.cache_data
 def load_data():
     df = pd.read_csv(DATA_PATH)
+
     df.sort_values(by="image_file", inplace=True)
     df.reset_index(drop=True, inplace=True)
+
     return df
