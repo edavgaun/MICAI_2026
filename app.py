191e15288d0e43aef2da45038c47f1764e3831a1
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2026-06-12 18:52:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from pages.Ecosystem_Setup import set_layout
 set_layout()
 
 pagina_home = st.Page("pages/Inicio.py", title="Home Overview", icon="🇲🇽", default=True)
-pagina_story = st.Page("pages/Story.py", title="Story of Mexican AI", icon="📖")
 pagina_grafo = st.Page("pages/Network.py", title="Institutional AI Network", icon="🕸️")
 
 def draw_custom_sidebar(pages):
@@ -15,12 +14,12 @@ def draw_custom_sidebar(pages):
         st.sidebar.page_link(page, label=page.title, icon=page.icon)
 
 nav = st.navigation(
-    [pagina_home, pagina_story, pagina_grafo],
+    [pagina_home, pagina_grafo],
     position="hidden"
 )
 
 draw_custom_sidebar(
-    [pagina_home, pagina_story, pagina_grafo]
+    [pagina_home, pagina_grafo]
 )
 
 nav.run()
