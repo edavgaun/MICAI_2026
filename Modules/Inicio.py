87dfefabe3df15a556fd7ecbc2ea66436853d7e9
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2026-06-13 11:25:34 -0500

    Create Inicio.py

---
+++ Modules/Inicio.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from Modules.Ecosystem_Setup import show_header, show_main_instructions, load_clean_data
+
+# Desplegamos tu contenido en la pantalla principal
+show_header("🇲🇽 Evolution of the Mexican AI Ecosystem")
+show_main_instructions()
+
+# Carga limpia de datos
+df = load_clean_data()
+st.sidebar.success(f"📈 Dataset ready: {df.shape[0]} records.")
